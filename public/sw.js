1ad4617c8ac203c9dde67954871f12e589269902
ricardoagronomia <ricardoagronomia53@gmail.com>
2026-02-01 20:20:01 -0300

    Delete public directory
    deletando

--- public/sw.js
+++
@@ -1,9 +0,0 @@
-self.addEventListener('fetch', event => {
-  if (event.request.url.includes('/collect.html')) {
-    event.respondWith(
-      caches.match(event.request).then(response => {
-        return response || fetch(event.request);
-      })
-    );
-  }
-});
